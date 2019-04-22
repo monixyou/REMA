10b53c0553928fa82cb0b70c1c68500f9b96e377
Raman Gupta <ramanxg@gmail.com>
2019-04-22 15:58:35 -0700

    button interacts with plane

---
+++ Assets/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public PlaneFinderBehaviour plane;
+    public Button buttonOnTheScene;
+
+    void Start()
+    {
+    
+        buttonOnTheScene.onClick.AddListener(TaskOnClick);
+    
+    }
+
+    void TaskOnClick()
+    {
+        Vector2 aPosition = new Vector2(0, 0);
+    
+        plane.PerformHitTest(aPosition);
+    }
+}
